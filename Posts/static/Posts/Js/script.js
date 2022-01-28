bd3adbda2330c0cdf97aebb6c7ff80e35895e694
samuelkaranja <samuelkaranja007@gmail.com>
2022-01-28 13:58:36 +0300

    Code update 28/01/2022

--- Posts/static/Posts/Js/script.js
+++
@@ -1,8 +0,0 @@
-<script>
-    $(document).ready(function() {
-        // messages timeout for 10 sec 
-        setTimeout(function() {
-            $('.alert').fadeOut('slow');
-        }, 10000) //time in milliseconds, 1000 =  1 sec
-    });
-</script>
